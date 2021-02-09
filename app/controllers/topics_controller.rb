7e0409f6fb3f89be25fd02f5f45d6a482a56810d
Junko Mikami <junkomikami312nz@gmail.com>
2021-02-09 14:23:56 +0900

    topic modelにもblog model と同じscopeを定義し、最新トピック３件のみ表示にした

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,7 +3,7 @@ class TopicsController < ApplicationController
 
   # GET /topics or /topics.json
   def index
-    @topics = Topic.all
+    @topics = Topic.latest
   end
 
   # GET /topics/1 or /topics/1.json
